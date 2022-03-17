3e61926655f1449f1952abadb1322dc3582203f5
markk <m.kors@rocmn.nl>
2022-03-17 13:37:25 +0100

    2e project demo added

---
+++ WpfAppDemo2/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppDemo2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static MainWindow mw;
+        public static string DBConnection;
+
+
+        public App()
+        {
+            Debug.WriteLine("Ik start op");
+            DBConnection = "Nieuwe connectie hier....";
+
+        }
+
+    }
+}
---
+++ WpfAppDemo2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public string naam;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            OpenWindow2.Click += OpenWindow2_Click;
+            App.mw = this;
+            Debug.WriteLine(App.DBConnection);
+        }
+
+        private void OpenWindow2_Click(object sender, RoutedEventArgs e)
+        {
+            naam = "waarde x";
+            SecondWindow sw = new SecondWindow();
+            sw.ShowDialog();
+
+
+        }
+    }
+}
